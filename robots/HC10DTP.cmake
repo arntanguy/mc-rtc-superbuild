80a7cb44bb3ecba2a282455038289a17509ffa47
Arnaud TANGUY <arn.tanguy@gmail.com>
2024-01-22 15:03:42 +0100

    [robots/HC10DTP] Add ros_control support

--- robots/HC10DTP.cmake
+++ robots/HC10DTP.cmake
@@ -1,4 +1,5 @@
 option(WITH_HC10DTP "Build Yaskawa HC10DTP support for mc_rtc" OFF)
+option(WITH_HC10DTP_ROS_CONTROL "Build the ros_control interface for controlling HC10DTP robot" OFF)
 
 if(NOT WITH_HC10DTP)
   return()
@@ -15,3 +16,24 @@ AddProject(mc_hc10dtp
   GIT_TAG origin/master
   DEPENDS hc10dtp_description mc_rtc
 )
+
+if(WITH_HC10DTP_ROS_CONTROL)
+  if(NOT WITH_HC10DTP)
+    message(FATAL_ERROR "Cannot build WITH_HC10DTP_ROS_CONTROL without WITH_HC10DTP=ON")
+  endif()
+
+  AptInstall(ros-${ROS_DISTRO}-industrial-msgs ros-${ROS_DISTRO}-industrial-robot-client)
+
+  AddCatkinProject(motoman
+    GITHUB ros-industrial/motoman
+    GIT_TAG origin/kinetic-devel
+    WORKSPACE mc_rtc_ws
+  )
+
+  AddCatkinProject(mc_rtc_ros_control_yaskawa
+    GITHUB arntanguy/mc_rtc_ros_control_yaskawa
+    GIT_TAG origin/main
+    WORKSPACE mc_rtc_ws
+    DEPENDS mc_rtc
+  )
+endif()
