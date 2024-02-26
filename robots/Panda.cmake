c9cea690b0af8e03e94ab1136eb546335a89d68d
Arnaud TANGUY <arn.tanguy@gmail.com>
2024-02-26 11:36:45 +0100

    [ROS2] Support franka_ros2

--- robots/Panda.cmake
+++ robots/Panda.cmake
@@ -11,19 +11,42 @@ if(NOT WITH_Panda)
 endif()
 
 if(Panda_DEPENDENCIES_FROM_SOURCE)
+  # The latest version of the robot server we can have at CNRS-LIRMM is 4.2.2
+  # According to the compability table: https://frankaemika.github.io/docs/compatibility.html
+  # This means that we can use libfranka >= 0.9.1 < 0.10.0
+  #
+  # However with any of these version the TSan unit test fails. Despite this
+  # it works on the real robot so we have simply disabled the unit tests here
   AddProject(libfranka
     GITHUB frankaemika/libfranka
-    GIT_TAG origin/0.8.0
+    GIT_TAG 0.9.2
+    CMAKE_ARGS -DBUILD_TESTS=OFF
   )
   set(mc_panda_DEPENDS libfranka)
   if(WITH_ROS_SUPPORT)
-    AddCatkinProject(franka_ros
-      GITHUB frankaemika/franka_ros
-      GIT_TAG origin/0.8.1
-      WORKSPACE mc_rtc_ws
-      DEPENDS libfranka
-    )
-    list(APPEND mc_panda_DEPENDS franka_ros)
+    if(ROS_IS_ROS2)
+      CreateCatkinWorkspace(
+	      ID franka_ws
+	      DIR catkin_ws_franka
+	      CATKIN_MAKE
+	      CATKIN_BUILD_ARGS --packages-skip joint_trajectory_controller franka_hardware franka_semantic_components franka_gripper franka_msgs franka_moveit_config franka_robot_state_broadcaster franka_example_controllers franka_bringup
+      )
+      AddCatkinProject(franka_ros2
+        GITHUB frankaemika/franka_ros2
+        GIT_TAG origin/humble
+        WORKSPACE franka_ws
+        DEPENDS libfranka
+      )
+      list(APPEND mc_panda_DEPENDS franka_ros2)
+    else()
+      AddCatkinProject(franka_ros
+        GITHUB frankaemika/franka_ros
+        GIT_TAG origin/0.8.1
+        WORKSPACE mc_rtc_ws
+        DEPENDS libfranka
+      )
+      list(APPEND mc_panda_DEPENDS franka_ros)
+    endif()
   endif()
 else()
   if(NOT DPKG OR NOT WITH_ROS_SUPPORT)
