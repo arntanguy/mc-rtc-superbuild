d5d02956d997cbbb5fb99692fdd0217d1549771d
Arnaud TANGUY <arn.tanguy@gmail.com>
2025-05-19 11:53:59 +0200

    maowen: pre-commit

--- extensions/local.cmake
+++ extensions/local.cmake
@@ -1,24 +1,25 @@
 AddProject(
-  mesh_sampling  
+  mesh_sampling
   GITHUB_PRIVATE jrl-umi3218/mesh_sampling
   GIT_TAG origin/master
   APT_PACKAGES mesh_sampling
-  APT_DEPENDENCIES libpcl-dev libassimp-dev libeigen3-dev libboost-program-options-dev libboost-filesystem-dev
+  APT_DEPENDENCIES libpcl-dev libassimp-dev libeigen3-dev libboost-program-options-dev
+                   libboost-filesystem-dev
 )
 
 AddProject(
   mc_robot_tools
   GITHUB_PRIVATE arntanguy/mc_robot_tools
-  GIT_TAG origin/topic/optional 
+  GIT_TAG origin/topic/optional
   DEPENDS mesh_sampling mc_rtc
-  APT_DEPENDENCIES qhull-bin 
+  APT_DEPENDENCIES qhull-bin
   CMAKE_ARGS -DWITH_INTERFACES=OFF -DWITH_TOOLS=OFF -DINSTALL_3rd_PARTY=OFF
 )
 
 AddProject(
   exo_hri
   GITE msun/exo_hri
-  GIT_TAG origin/main 
+  GIT_TAG origin/main
 )
 
 AddProject(
@@ -35,21 +36,24 @@ AddProject(
 # )
 
 # Human model
-AddCatkinProject(human_description
+AddCatkinProject(
+  human_description
   GITE hlefevre/human_description
   GIT_TAG origin/master
   WORKSPACE data_ws
 )
 
 # Robot module for human model
-AddProject(mc_human
+AddProject(
+  mc_human
   GITE hlefevre/mc_human
   GIT_TAG origin/master
   DEPENDS human_description mc_rtc
 )
 
 # mc-rtc-magnum
-AddProject(mc_rtc-magnum
+AddProject(
+  mc_rtc-magnum
   GITHUB arntanguy/mc_rtc-magnum
   GIT_TAG origin/main
   DEPENDS mc_rtc
